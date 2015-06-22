bbcb1f7e028ecb574cf0a57bf8b521ec2870c9ad
Thalita <thalitafdrumond@gmail.com>
2015-06-22 14:06:59 -0300

    Separated parts of exp 1: BMF generation, training and testing. gen_BFM will work for other exps

--- exp_1_test.py
+++ exp_1_test.py
@@ -1,38 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu May 21 14:32:12 2015
+Created on Sun Jun 21 20:04:53 2015
 
 @author: thalita
-
-Experiment 1
-
-Reproduce BMF results from Nenova et al (2013)
 """
 
 import data.MovieLens100k.dbread as dbread
 from databases import MatrixDatabase
 from evaluation import HoldoutBMF, HoldoutRatingsView
 import recommender as rec
-from multiprocessing import Pool
 from itertools import chain
 import os
 import sys
 import traceback
-
 
 if 'parallel' in set(sys.argv):
     PARALLEL = True
 else:
     PARALLEL = False
 
-result_folder = 'results/exp_1_results/train/'
+result_folder = 'results/exp_1_results/test/'
 RS_type = rec.BMFrecommender
 
 coverages = [1, 0.8, 0.6]
 bin_thresh = [i for i in range(0, 5)]
 
 RS_arguments = [{'neighbor_type': nn_type,
-                 'offline_kNN': False,
+                 'offline_kNN': offline,
                  'n_neighbors': nn,
                  'algorithm': 'brute',
                  'metric': 'cosine',
@@ -41,6 +35,7 @@ RS_arguments = [{'neighbor_type': nn_type,
                 for nn_type in ['user', 'item']
                 for nn in chain([5], range(10, 61, 10))
                 for t in bin_thresh
+                for offline in [True, False]
                 for coverage in coverages]
 
 
@@ -53,27 +48,15 @@ if not os.path.isdir(result_folder):
 
 
 def run(i):
-    global kfold_view, RS_type, RS_arguments, result_folder
+    global holdout_view, RS_type, RS_arguments, result_folder
     print('Running %d' % i + str(RS_arguments[i]))
     evalu = HoldoutBMF(holdout_view, RS_type, RS_arguments[i],
                        result_folder, threshold=3, topk=20)
     try:
-
-        print('Training %d' % i + str(RS_arguments[i]))
-        evalu.train()
-        print('Done training %d' % i + str(RS_arguments[i]))
-
         print('Testing %d' % i + str(RS_arguments[i]))
-        evalu.test()
+        evalu.test(parallel=PARALLEL)
         print('Done testing %d' % i + str(RS_arguments[i]))
 
     except:
         with open(evalu.fname_prefix+'_error_log_%d.out' % i, 'w') as f:
             traceback.print_exception(*sys.exc_info(), file=f)
-
-if PARALLEL:
-    pool = Pool()
-    pool.map(run, range(len(RS_arguments)))
-else:
-    for i in range(len(RS_arguments)):
-        run(i)
