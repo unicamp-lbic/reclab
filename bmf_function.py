396d78ee6f45fe7b455251560a0866ca056ca1d2
Thalita Firmo Drumond <thalitafdrumond@gmail.com>
2016-02-12 16:49:45 -0200

    bmf_function: Improvments for sparse matrices and algorithm fix
    Made some small efficiency improvments for handling sparse matrices.
    Noticed an error in the algorithm implementation, when doing concept
    generation: should always use the original data matrix for this,
    not the remaining_pairs matrix.
    Also added a small progress print.

--- bmf_function.py
+++ bmf_function.py
@@ -24,16 +24,24 @@ class Concept(object):
 concepts = None
 curr_concept = None
 remaining_pairs = None
+remaining_pairs_csr = None
 matrix = None
 new_coverage = None
 
 def coverage(concept):
-    global remaining_pairs
-    return remaining_pairs[list(concept.extent), :]\
+    global remaining_pairs, remaining_pairs_csr
+    if len(concept.extent) == 0 or len(concept.intent)==0:
+        return 0
+    if sp.issparse(remaining_pairs):
+        return remaining_pairs_csr[list(concept.extent), :].tocsc()\
+        [:, list(concept.intent)].sum()
+    else:
+        return remaining_pairs[list(concept.extent), :]\
         [:, list(concept.intent)].sum()
 
 def gen_possible_concept(j):
-    global concepts, curr_concept, remaining_pairs, matrix, new_coverage
+    global concepts, curr_concept, remaining_pairs, remaining_pairs_csr,\
+           matrix, new_coverage
     n, m = matrix.shape
     # Generate possible concepts
     if j not in curr_concept.intent:
@@ -49,33 +57,41 @@ def gen_possible_concept(j):
                 set([obj for obj in range(n)
                  if matrix[obj, j] == 1])
         # Update intent
-        update_intent = np.ones((1,m))
-        for obj in concepts[j].extent:
-            if sp.issparse(remaining_pairs):
-                update_intent *= remaining_pairs[obj, :].toarray()
-            else:
-                update_intent *= remaining_pairs[obj, :]
-            if update_intent.sum() == 0:
-                break
-        update_intent = [i for i, val in enumerate(update_intent[0,:])
-                        if val == 1]
+        if sp.issparse(remaining_pairs):
+            update_intent = set(range(m))
+            for obj in concepts[j].extent:
+                update_intent = update_intent.intersection(
+                    matrix[obj, :].indices)
+                if len(update_intent) == 0:
+                    break
+        else:
+            update_intent = np.ones((1,m))
+            for obj in concepts[j].extent:
+                update_intent *= matrix[obj, :]
+                if update_intent.sum() == 0:
+                    break
+            update_intent = [i for i, val in enumerate(update_intent[0,:])
+                            if val == 1]
         concepts[j].intent.update(update_intent)
         # calculate coverage
         new_coverage[j] = coverage(concepts[j])
 
 def bmf(amatrix, min_coverage=1.0):
-    global concepts, curr_concept, remaining_pairs, matrix, new_coverage
+    global concepts, curr_concept, remaining_pairs, remaining_pairs_csr,\
+           matrix, new_coverage
     matrix = amatrix
     n, m = matrix.shape
-
+    total = matrix.sum()
     if sp.issparse(matrix):
         remaining_pairs = sp.lil_matrix(matrix)
+        remaining_pairs_csr = remaining_pairs.tocsr()
+        matrix = matrix.tocsr()
     else:
         remaining_pairs = matrix.copy()
-
-    max_uncoverage = (1 - min_coverage) * remaining_pairs.sum()
+    remaining_sum = remaining_pairs.sum()
+    max_uncoverage = (1 - min_coverage) * remaining_sum
     factors = set()
-    while remaining_pairs.sum() > max_uncoverage :
+    while remaining_sum > max_uncoverage :
         curr_concept = Concept()
         curr_coverage = 0
         # look greedily for maximal coverage concept
@@ -100,5 +116,11 @@ def bmf(amatrix, min_coverage=1.0):
         for i in curr_concept.extent:
             for j in curr_concept.intent:
                 remaining_pairs[i, j] = 0
+        if sp.issparse(matrix):
+            remaining_pairs_csr = remaining_pairs.tocsr()
+        remaining_sum = remaining_pairs.sum()
+        print('%d remaining (of %d)' % (remaining_sum, total),
+              end='\r', flush=True)
 
     return factors
+
